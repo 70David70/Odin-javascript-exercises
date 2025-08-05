fcf0f55b05e2298a67601bb8f6f22b9c4866e462
Daivd <drgdeved@gmail.com>
2025-08-05 19:10:20 +0300

    Finished problem number 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+   let joinedString = []
+   for(let i = 0; i < num; i++) {
+    joinedString.push(string);
+   }
+   return joinedString.join('')
 };
 
 // Do not edit below this line
