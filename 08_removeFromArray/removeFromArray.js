dd38eac7f1c277543a675f864927f6ee28329af2
Daivd <drgdeved@gmail.com>
2025-08-06 05:23:45 +0300

    Finished problem 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    let indices  = []
+    for (num of nums) {
+        indices .push(array.indexOf(num))
+    }
+    let numberOfElements = nums.length
+    array.splice(indices , numberOfElements)
+    return array
 };
+console.log(removeFromArray([1, 2, 3], 1, 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
