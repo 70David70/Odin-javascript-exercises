15020a9f3ca04a8b50f24b158fcfe5104750016b
Daivd <drgdeved@gmail.com>
2025-08-08 05:11:27 +0300

    Finished problem 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  //return fahr % 2 === 0 ? fahr - 32 : (fahr - 32).toFixed(1)
+  let converted = (fahr - 32) * 5 / 9;
+  return converted % 2 === 0 ? parseFloat(converted) : parseFloat(converted.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  // return cel % 2 === 0 ? cel + 32 : (cel + 32).toFixed(1)
+  let converted = (cel * 9 / 5) + 32;
+  return converted % 2 === 0 ? parseFloat(converted) : parseFloat(converted.toFixed(1))
 };
 
 // Do not edit below this line
