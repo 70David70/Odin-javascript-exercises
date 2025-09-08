15d56348be98dc1f34fdaf7363335bf4cf4fb4cf
Daivd <drgdeved@gmail.com>
2025-09-09 00:32:04 +0300

    Finish problem number 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let oldestAge = 0;
+    let oldest;
+    obj.forEach(person => {
+        let currentAge;
+        if (person.yearOfDeath) {
+            currentAge = person.yearOfDeath - person.yearOfBirth;
+        }
+        else{
+            currentAge = (new Date().getFullYear()) - person.yearOfBirth;
+            console.log(currentAge)
+        }
 
+        if ( currentAge > oldestAge) {
+            oldest = person;
+            oldestAge = currentAge;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
