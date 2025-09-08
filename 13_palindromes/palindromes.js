cd975577601dff6615000d441a616bb656880931
Daivd <drgdeved@gmail.com>
2025-09-08 22:34:30 +0300

    Finish problem number 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let array = string.toLowerCase().match(/[a-z0-9]/gi);
+    let j = array.length - 1;
 
+    for (let i = 0; i < j; i++, j--) {
+        if(array[i] !== array[j]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
