a47813580ae592ab4df9bba326443d6bea967805
Daivd <drgdeved@gmail.com>
2025-09-08 23:01:59 +0300

    Finish problem number 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(length) {
+    if (length < 0) return "OOPS";
+    else if (length == 0) return 0;
+    
+    let numbers = [1];
+    for (let i = 1; i < length; i++) {
+        if (i == 1) numbers.push(1)
+        else numbers.push((numbers[i - 1] + numbers[i - 2]))
+    }
+    return numbers[length - 1];
 };
 
 // Do not edit below this line
